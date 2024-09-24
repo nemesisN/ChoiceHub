b5acb25ac3878ed66f687053e10eb00b7e46d433
nemesis_S <shaikhnaushadofficial@gmail.com>
2024-09-24 11:16:02 +0530

    login added

--- ChoiceHub/ChoiceHub/urls.py
+++ ChoiceHub/ChoiceHub/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for ChoiceHub project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
@@ -25,4 +25,4 @@ urlpatterns = [
     # path('', views.index, name='index'),
     # path('register/', views.register, name='register'),
     # path('quiz/', book_recommendation, name='book_recommendation'),
-] 
+]
